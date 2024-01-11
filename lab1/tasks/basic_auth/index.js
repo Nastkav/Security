5788015c717149268be2d944f1f8f9a91af5f32d
Nastkav <76096236+Nastkav@users.noreply.github.com>
2024-01-11 17:22:29 +0200

    Add files via upload

---
+++ lab1/tasks/basic_auth/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("\n=======================================================\n");
+
+  const authorizationHeader = req.get("Authorization");
+  console.log("authorizationHeader", authorizationHeader);
+
+  if (!authorizationHeader) {
+    res.setHeader("WWW-Authenticate", 'Basic realm="Ukraine"'); // realm/promt
+    res.status(401);
+    res.send("Unauthorized");
+    return;
+  }
+
+  const authorizationBase64Part = authorizationHeader.split(" ")[1];
+  console.log("authorizationBase64Part", authorizationBase64Part);
+
+  const decodedAuthorizationHeader = Buffer.from(
+    authorizationBase64Part,
+    "base64"
+  ).toString("utf-8");
+  console.log("decodedAuthorizationHeader", decodedAuthorizationHeader);
+
+  const login = decodedAuthorizationHeader.split(":")[0];
+  const password = decodedAuthorizationHeader.split(":")[1];
+  console.log("Login/Password", login, password);
+
+  if (login == "login" && password == "1111") {
+    req.login = login;
+    return next();
+  }
+
+  res.setHeader("WWW-Authenticate", 'Basic realm="Ukraine"');
+  res.status(401);
+  res.send("Unauthorized");
+});
+
+app.get("/", (req, res) => {
+  res.send(`Hello ${req.login}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
